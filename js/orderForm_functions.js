fad248f3eedb2be84b7bef545953fbdba091f4be
MAtt5816 <m.szewczyk58@gmail.com>
2021-06-14 17:48:08 +0200

    added quantity selector into menu positions in order form

--- js/orderForm_functions.js
+++ js/orderForm_functions.js
@@ -1,20 +1,37 @@
 $(document).ready(function(){
   $('input[type="checkbox"]').click(function(){
     var id = $(this).prop('id');
+    var div = $('div#div_'+id+'.menuPosition');
+
     var color = $('div#div_'+id+'.menuPosition').css('border-color');
     var style = {};
+    var code = div.children().first().html();
+    var span_quantity = '<span class="quantity"><input type="number" name="'+id+'" id="qt_'+id+'" min="1" value="1"></span>';
+
     if (color == "green" || color == "#008000" || color == "rgb(0, 128, 0)"){
       style = {
-        borderColor: "black",
+        borderColor: "#3D2B05",
         borderWidth: "2px"
       };
+
+      $('#'+id+'.dish_name').css('flex', '80%');
+      code = code.replace(span_quantity, '');
     }
     else {
       style = {
         borderColor: "green",
-        borderWidth: "5px"
+        borderWidth: "3px"
       };
+
+      $('#'+id+'.dish_name').css('flex', '65%');
+      code = span_quantity+code;
     }
-    $('div#div_'+id+'.menuPosition').css(style);
+
+
+    div.css(style);
+
+
+
+    div.children().first().html(code);
   });
 });
